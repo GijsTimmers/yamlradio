834d0670954dba6136351c57a3a2db408cad0edd
GijsTimmers <timmers.gijs@gmail.com>
2015-02-17 15:12:18 +0100

    added comments, improved structure

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # radio
 A small Python script to play various radio stations from a terminal.
+
+## Dependencies:
+- `mplayer`
+- `argparse`
+
+You can install mplayer via your package manager. 
+You can install argparse via `pip`:
+
+    sudo pip install argparse
--- radio.py
+++ radio.py
@@ -1,36 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-#
-#  radio.py
-#  
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
 
+## Dependencies:    argparse, mplayer
+## Author:          Gijs Timmers: https://github.com/GijsTimmers
 
-import sys
-import argparse                         ## Parst argumenten
-import subprocess
+## Licence:         CC-BY-SA-4.0
+##                  http://creativecommons.org/licenses/by-sa/4.0/
+
+## This work is licensed under the Creative Commons
+## Attribution-ShareAlike 4.0 International License. To  view a copy of
+## this license, visit http://creativecommons.org/licenses/by-sa/4.0/ or
+## send a letter to Creative Commons, PO Box 1866, Mountain View,
+## CA 94042, USA.
+
+import sys                      ## Basislib
+import argparse                 ## Parst argumenten
+import subprocess               ## Om programma's uit te voeren vanuit Python
 
 class Radio():
     def __init__(self):
+        ## Parser instantiëren
         self.parser = argparse.ArgumentParser()
+        
+        ## Zorgen dat geen twee zenders tegelijk kunnen worden ingevoerd
         self.zenderparser = self.parser.add_mutually_exclusive_group()
         
+        ## Mogelijke zenders opgeven
         self.zenderparser.add_argument("--stubru",
         help="Studio Brussel",\
         action="store_const", dest="zender", const="stubru")
@@ -47,27 +42,59 @@ class Radio():
         help="RiseFM",\
         action="store_const", dest="zender", const="risefm")
         
-    def afspelen(self):
+        self.zenderparser.add_argument("--r2nl",
+        help="Radio 2 (Nederland)",\
+        action="store_const", dest="zender", const="r2nl")
+        
+        
+        
+    def zendervinden(self):
+        ## De ingevoerde argumenten parsen
         argumenten = self.parser.parse_args()
+        
+        ## Zenderlijst definiëren; voeg hier extra zenders toe als
+        ## een zender:url-dictionary
         
         zenderlijst = {
         "stubru": "http://mp3.streampower.be/stubru-high.mp3",
         "538"   : "http://82.201.100.9:8000/radio538",
         "3fm"   : "http://icecast.omroep.nl/3fm-bb-mp3",
-        "risefm": "http://mastersound.hu/clubfmplay_hq"
+        "risefm": "http://mastersound.hu/clubfmplay_hq",
+        "r2nl"  : "http://icecast.omroep.nl/radio2-bb-mp3"
         }
         
-        url = zenderlijst[argumenten.zender]
-        print "Speelt nu af: " + argumenten.zender + ". " + \
+        
+        ## Indien er geen opties zijn meegegeven, krijgen we een KeyError.
+        ## In dat geval printen we de --help-output.
+        try:
+            url = zenderlijst[argumenten.zender]
+            return (argumenten.zender, url)
+        except KeyError:
+            self.parser.parse_args("--help".split())
+        
+        
+        
+    def afspelen(self, zender, url):
+        print "Speelt nu af: " + zender + ". " + \
         "Druk op Q om te beëindigen."
         
-        
-        dev_null = open("/dev/null", "w")
-        subprocess.call(["mplayer", url], stdout = dev_null, stderr = dev_null)
+        ## dev_null als schrijfbestand definiëren om output te verbergen.
+        try:
+            dev_null = open("/dev/null", "w")
+        except IOError:
+            print "Kon /dev/null niet bereiken."
+            
+        ## mplayer starten
+        try:
+            subprocess.call(["mplayer", url], \
+            stdout = dev_null, stderr = dev_null)
+        except OSError:
+            print "Kon geen mplayer-executable vinden in $PATH."
             
 def main():
     rd = Radio()
-    rd.afspelen()
+    (zender, url) = rd.zendervinden()
+    rd.afspelen(zender, url)
     return 0
 
 if __name__ == '__main__':
