71f3dd187b662b6fbf1772d89d0c2f2a5fd25719
GijsTimmers <timmers.gijs@gmail.com>
2015-03-03 01:53:38 +0100

    separated name from id

--- radio.py
+++ radio.py
@@ -14,50 +14,25 @@
 ## send a letter to Creative Commons, PO Box 1866, Mountain View,
 ## CA 94042, USA.
 
+import os                       ## Basislib
 import sys                      ## Basislib
-import yaml
+import yaml                     ## Configuratie inlezen
 import argparse                 ## Parst argumenten
 import argcomplete              ## Argumenten aanvullen met Tab
 import subprocess               ## Om programma's uit te voeren vanuit Python
 
 class Radio():
     def __init__(self):
-        
         ## Zenderdictionary aanmaken
-        with open("zenders.yaml", "r") as f:
+        with open(os.path.join(os.path.dirname(__file__), 
+        "zenders.yaml"), "r") as f:
             self.zenderdict = yaml.load(f)
         
         ## Parser instantiëren
         self.parser = argparse.ArgumentParser()
         self.parser.add_argument('zender', choices=self.zenderdict.keys())
         
-        
-        """
-        ## Zorgen dat geen twee zenders tegelijk kunnen worden ingevoerd
-        self.zenderparser = self.parser.add_mutually_exclusive_group()
-        
-        ## Mogelijke zenders opgeven
-        self.zenderparser.add_argument("--stubru",
-        help="Studio Brussel",\
-        action="store_const", dest="zender", const="stubru")
-        
-        self.zenderparser.add_argument("--538",
-        help="Radio 538",\
-        action="store_const", dest="zender", const="538")
-        
-        self.zenderparser.add_argument("--3fm",
-        help="3FM",\
-        action="store_const", dest="zender", const="3fm")
-        
-        self.zenderparser.add_argument("--risefm",
-        help="RiseFM",\
-        action="store_const", dest="zender", const="risefm")
-        
-        self.zenderparser.add_argument("--r2nl",
-        help="Radio 2 (Nederland)",\
-        action="store_const", dest="zender", const="r2nl")
-        """
-        
+                
         ## Argumenten automatisch aanvullen met TAB.
         argcomplete.autocomplete(self.parser)
         
@@ -70,8 +45,9 @@ class Radio():
         ## Indien er geen opties zijn meegegeven, krijgen we een KeyError.
         ## In dat geval printen we de --help-output.
         try:
-            url = self.zenderdict[argumenten.zender]
-            return (argumenten.zender, url)
+            naam = self.zenderdict[argumenten.zender]["naam"]
+            url  = self.zenderdict[argumenten.zender]["url"]
+            return (naam, url)
         except KeyError:
             self.parser.parse_args("--help".split())
         
@@ -94,8 +70,8 @@ class Radio():
             
 def main():
     rd = Radio()
-    (zender, url) = rd.zendervinden()
-    rd.afspelen(zender, url)
+    (naam, url) = rd.zendervinden()
+    rd.afspelen(naam, url)
     return 0
 
 if __name__ == '__main__':
