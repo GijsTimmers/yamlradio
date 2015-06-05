0059a472553bf09e0508c3e7ccdc21aacf3a6a1d
GijsTimmers <timmers.gijs@gmail.com>
2015-06-05 13:43:13 +0200

    updated playing mechanism, using threads now

--- radio.py
+++ radio.py
@@ -18,6 +18,8 @@ import re                       ## Regex
 import os                       ## Basislib
 import sys                      ## Basislib
 import yaml                     ## Configuratie inlezen
+import time
+import threading
 import argparse                 ## Parst argumenten
 import argcomplete              ## Argumenten aanvullen met Tab
 import subprocess               ## Om programma's uit te voeren vanuit Python
@@ -46,35 +48,62 @@ class Radio():
         return (naam, url)
 
     def afspelen(self, zender, url):
-        try:
-            ## dev_null als schrijfbestand definiëren om output te verbergen.
-            with open(os.devnull, "w") as dev_null:
-                self.stream = subprocess.Popen(["mplayer", url], \
-                stdout = dev_null, stderr = dev_null)
-                
-                ## We encoderen de zendernaam in UTF-8 om errors te voorkomen
-                ## in de stringnaam: "België" zou anders een probleem geven.
-                print "Speelt nu af: {zender}. " \
-                      "Druk op Enter om te beëindigen." \
-                      .format(zender=zender.encode("utf-8"))
-                      
-        except IOError:
-            print "Kon /dev/null niet bereiken."
+        try:        
+            self.stream = subprocess.Popen(["mplayer", url], \
+            stdin=subprocess.PIPE, \
+            stdout=subprocess.PIPE, \
+            stderr=subprocess.STDOUT, \
+            bufsize=1, \
+            universal_newlines=True)
+            
+            ## We encoderen de zendernaam in UTF-8 om errors te voorkomen
+            ## in de stringnaam: "België" zou anders een probleem geven.
+            print "Speelt nu af: {zender}. " \
+                  "Druk op Enter om te beëindigen." \
+                  .format(zender=zender.encode("utf-8"))
+            
+            for regel in iter(self.stream.stdout.readline, ''):
+                ## Per nieuwe entry in stdout.readline wordt door deze loop
+                ## gegaan. Als bijvoorbeeld de ICY-info verandert, wordt er
+                ## opnieuw geprint: ICY Info: ... Dat wordt opgepakt door de if,
+                ## en geprint. Zo hebben we iedere keer de meest recente
+                ## info te pakken.
+                if re.match("^ICY", regel):
+                    info = re.findall("(?<=ICY Info: StreamTitle=').*(?=';)", regel)[0]
+                    print "Info: [{info}]".format(info=info)
+                    pass
+                if re.match("^Exiting...", regel):
+                    break
+            return()
+            
         except OSError:
             print "Kon geen mplayer-executable vinden in $PATH."
-    
+        
     def stoppen(self):
-        self.stream.kill()
+        ## Stuur de toetsindruk Q naar de stream. mplayer reageert op q
+        ## door te stoppen. Bij het stoppen print mplayer "Exiting...". In
+        ## de thread t loopt een for-loop, welke deze string opvangt. Als 
+        ## reactie stopt de for-loop ('break'), en komen we aan bij het
+        ## return()-statenment. De thread is nu beëindigd.
+        self.stream.communicate(input="q")
+        
 
 
 def main():
     rd = Radio()
     naam, url = rd.zendervinden()
-    rd.afspelen(naam, url)
+    t = threading.Thread(target=rd.afspelen, args=(naam, url))
+    t.start()
+    
     ## Afspelen stoppen na drukken op ENTER mbv raw_input(): het script kan 
     ## pas verder na een invoer bij raw_input() en blijft daardoor afspelen.
-    raw_input()
-    rd.stoppen()
+    try:
+        raw_input()
+        rd.stoppen()
+        t.join()
+    except KeyboardInterrupt:
+        print "\nAfsluiten kan ook met Enter."
+        
     return 0
 
 if __name__ == '__main__':
