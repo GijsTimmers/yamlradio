748376dd43ec4f6b414291cff428df083be8c6c0
GijsTimmers <timmers.gijs@gmail.com>
2015-09-28 01:44:40 +0200

    moved __main__.py's contents to __init__.py, redirected startup files

--- yamlradio-runner.py
+++ yamlradio-runner.py
@@ -14,7 +14,7 @@
 ## send a letter to Creative Commons, PO Box 1866, Mountain View,
 ## CA 94042, USA.
 
-from yamlradio.yamlradio import main
+from yamlradio import rd
 
 if __name__ == "__main__":
-    main()
+    rd()
