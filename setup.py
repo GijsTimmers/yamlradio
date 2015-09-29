c975d511ab2268d3677ba1e1f90f758ab78fa6ee
GijsTimmers <timmers.gijs@gmail.com>
2015-09-29 20:24:48 +0200

    excluded *.pyc files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
   name = "yamlradio",
   packages = ["yamlradio"],
-  version = "1.4.3",
+  version = "1.4.4",
   description = "A small Python package to play radio stations as defined in a YAML file.",
   author = "Gijs Timmers",
   author_email = "gijs.timmers@student.kuleuven.be",
