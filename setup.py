c2f4bf3d8bfbe1816c1b52925304c02b1be185fb
GijsTimmers <timmers.gijs@gmail.com>
2018-03-27 14:00:44 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ try:
     setup(
     name = "yamlradio",
     packages = ["yamlradio"],
-    version = "2.2.1",
+    version = "2.2.2",
     description = "A small Python3 package to play radio stations as defined " +
         "in a YAML file.",
     author = "Gijs Timmers",
