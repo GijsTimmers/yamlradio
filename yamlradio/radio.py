a960ddfb436cf81a18ac0313e610adc03778a45d
GijsTimmers <timmers.gijs@gmail.com>
2018-03-29 23:01:32 +0200

    disabled caching

--- yamlradio/radio.py
+++ yamlradio/radio.py
@@ -22,7 +22,7 @@ import tempfile                 ## Voor tijdelijke mappen / bestanden
 
 class Radio(object):
     def __init__(self):
-        self.cmd = ["mpv", "--no-terminal", "--no-ytdl"]
+        self.cmd = ["mpv", "--no-terminal", "--no-ytdl", "--no-cache"]
 
     def afspelen(self, url):
         dev_null = open(os.devnull, "w")
